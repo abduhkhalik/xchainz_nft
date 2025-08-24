c9e771fecc2be5696e88a4d5820029a74cd6237d
abduhkhalik <halikparestu11@gmail.com>
2025-08-24 20:31:35 +0800

    update

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
 
 // app/auth/error/page.tsx
-export default function AuthErrorPage({
+export default async function AuthErrorPage({
   searchParams,
 }: {
-  searchParams: { error?: string };
+  searchParams: Promise<Record<string, string | string[] | undefined>>;
 }) {
-  const error = searchParams?.error;
+  const params = await searchParams;
+  const error = params?.error as string | undefined;
 
   let message = "Login gagal, silakan coba lagi.";
+
   if (error === "NOT_HOLDER") {
     message = "❌ Anda bukan holder koleksi NFT, akses ditolak.";
   } else if (error === "CredentialsSignin") {
@@ -19,12 +21,12 @@ export default function AuthErrorPage({
     <div className="flex h-screen items-center justify-center">
       <div className="p-6 bg-white shadow rounded text-center">
         <h1 className="text-xl font-bold mb-2">Login Error</h1>
-        <p>{message}</p>
+        <p className="mb-4">{message}</p>
         <Link
-          href={"/"}
-          className="text-3xl text-white px-4 py-2 rounded-full bg-blue-600 hover:bg-blue-700"
+          href="/"
+          className="px-4 py-2 rounded-full bg-blue-600 hover:bg-blue-500 text-white"
         >
-          BACK
+          BACK HOME
         </Link>
       </div>
     </div>
