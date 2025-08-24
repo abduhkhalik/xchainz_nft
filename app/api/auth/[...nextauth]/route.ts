903ee4bed731fc91af94634e9fc0b283b2d76bff
abduhkhalik <halikparestu11@gmail.com>
2025-08-24 18:57:10 +0800

    update

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -27,7 +27,7 @@ const handler = NextAuth({
     }),
   ],
   session: { strategy: "jwt" },
-  pages: { signIn: "/", error: "/api/auth/error" },
+  pages: { signIn: "/", error: "/auth/error" },
 });
 
 // ⬅️ WAJIB: export GET & POST agar NextAuth jalan
